77d747ced8304464dbf76d28360ce3217521f446
Alexandria <walkerciara24@gmail.com>
2025-01-08 16:57:26 -0500

    Update AuthContext path

--- frontend/src/context/RecipeContext.jsx
+++ frontend/src/context/RecipeContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from 'react';
 import { api } from '../lib/axios';
-import { useAuth } from './authContext';
+import { useAuth } from './AuthContext';
 
 const RecipeContext = createContext(null);
 
